580f3b7374ebd42a8f813ba11ecb6c7229614712
Stefan Lehmann <stefan.lehmann@hpi.de>
2016-08-30 20:07:41 +0200

    first running example of box plot

--- index.js
+++ index.js
@@ -6,7 +6,91 @@ var height = 400 - margin.top - margin.bottom;
 	
 var min = Infinity,
     max = -Infinity;
-	
+
+function doViz(data) {
+	document.querySelector('#chart') && document.querySelector('#chart').remove();
+	var parent = document.createElement('div');
+	parent.id = 'chart';
+	document.body.appendChild(parent);
+
+	max = data.reduce((acc, dat) => Math.max(acc, dat[1].reduce((acc, num) => Math.max(acc, num), -Infinity
+	)), -Infinity);
+	min = data.reduce((acc, dat) => Math.min(acc, dat[1].reduce((acc, num) => Math.min(acc, num), Infinity
+	)), Infinity);
+
+	var chart = d3.box()
+		.whiskers(iqr(1.5))
+		.height(height)
+		.domain([min, max])
+		.showLabels(labels);
+
+	var svg = d3.select("#chart").append("svg")
+		.attr("width", width + margin.left + margin.right)
+		.attr("height", height + margin.top + margin.bottom)
+		.attr("class", "box")
+		.append("g")
+		.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+	// the x-axis
+	var x = d3.scale.ordinal()
+		.domain( data.map(function(d) { console.log(d); return d[0] } ) )
+		.rangeRoundBands([0 , width], 0.7, 0.3);
+
+	var xAxis = d3.svg.axis()
+		.scale(x)
+		.orient("bottom");
+
+	// the y-axis
+	var y = d3.scale.linear()
+		.domain([min, max])
+		.range([height + margin.top, 0 + margin.top]);
+
+	var yAxis = d3.svg.axis()
+		.scale(y)
+		.orient("left");
+
+	// draw the boxplots
+	svg.selectAll(".box")
+		.data(data)
+		.enter().append("g")
+		.attr("transform", function(d) { return "translate(" +  x(d[0])  + "," + margin.top + ")"; } )
+		.call(chart.width(x.rangeBand()));
+
+
+	// add a title
+	svg.append("text")
+		.attr("x", (width / 2))
+		.attr("y", 0 + (margin.top / 2))
+		.attr("text-anchor", "middle")
+		.style("font-size", "18px")
+		//.style("text-decoration", "underline")
+		.text("Revenue 2012");
+
+	// draw y axis
+	svg.append("g")
+		.attr("class", "y axis")
+		.call(yAxis)
+		.append("text") // and text1
+		.attr("transform", "rotate(-90)")
+		.attr("y", 6)
+		.attr("dy", ".71em")
+		.style("text-anchor", "end")
+		.style("font-size", "16px")
+		.text("Revenue in €");
+
+	// draw x axis
+	svg.append("g")
+		.attr("class", "x axis")
+		.attr("transform", "translate(0," + (height  + margin.top + 10) + ")")
+		.call(xAxis)
+		.append("text")             // text label for the x axis
+		.attr("x", (width / 2) )
+		.attr("y",  10 )
+		.attr("dy", ".71em")
+		.style("text-anchor", "middle")
+		.style("font-size", "16px")
+		.text("Quarter");
+}
 // parse in the data	
 d3.csv("data.csv", function(error, csv) {
 	// using an array of arrays with
@@ -40,92 +124,15 @@ d3.csv("data.csv", function(error, csv) {
 			v3 = Math.floor(x.Q3),
 			v4 = Math.floor(x.Q4);
 			// add more variables if your csv file has more columns
-			
-		var rowMax = Math.max(v1, Math.max(v2, Math.max(v3,v4)));
-		var rowMin = Math.min(v1, Math.min(v2, Math.min(v3,v4)));
 
 		data[0][1].push(v1);
 		data[1][1].push(v2);
 		data[2][1].push(v3);
 		data[3][1].push(v4);
 		 // add more rows if your csv file has more columns
-		 
-		if (rowMax > max) max = rowMax;
-		if (rowMin < min) min = rowMin;	
 	});
-  
-	var chart = d3.box()
-		.whiskers(iqr(1.5))
-		.height(height)	
-		.domain([min, max])
-		.showLabels(labels);
 
-	var svg = d3.select("body").append("svg")
-		.attr("width", width + margin.left + margin.right)
-		.attr("height", height + margin.top + margin.bottom)
-		.attr("class", "box")    
-		.append("g")
-		.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-	
-	// the x-axis
-	var x = d3.scale.ordinal()	   
-		.domain( data.map(function(d) { console.log(d); return d[0] } ) )	    
-		.rangeRoundBands([0 , width], 0.7, 0.3); 		
-
-	var xAxis = d3.svg.axis()
-		.scale(x)
-		.orient("bottom");
-
-	// the y-axis
-	var y = d3.scale.linear()
-		.domain([min, max])
-		.range([height + margin.top, 0 + margin.top]);
-	
-	var yAxis = d3.svg.axis()
-    .scale(y)
-    .orient("left");
-
-	// draw the boxplots	
-	svg.selectAll(".box")	   
-      .data(data)
-	  .enter().append("g")
-		.attr("transform", function(d) { return "translate(" +  x(d[0])  + "," + margin.top + ")"; } )
-      .call(chart.width(x.rangeBand())); 
-	
-	      
-	// add a title
-	svg.append("text")
-        .attr("x", (width / 2))             
-        .attr("y", 0 + (margin.top / 2))
-        .attr("text-anchor", "middle")  
-        .style("font-size", "18px") 
-        //.style("text-decoration", "underline")  
-        .text("Revenue 2012");
- 
-	 // draw y axis
-	svg.append("g")
-        .attr("class", "y axis")
-        .call(yAxis)
-		.append("text") // and text1
-		  .attr("transform", "rotate(-90)")
-		  .attr("y", 6)
-		  .attr("dy", ".71em")
-		  .style("text-anchor", "end")
-		  .style("font-size", "16px") 
-		  .text("Revenue in €");		
-	
-	// draw x axis	
-	svg.append("g")
-      .attr("class", "x axis")
-      .attr("transform", "translate(0," + (height  + margin.top + 10) + ")")
-      .call(xAxis)
-	  .append("text")             // text label for the x axis
-        .attr("x", (width / 2) )
-        .attr("y",  10 )
-		.attr("dy", ".71em")
-        .style("text-anchor", "middle")
-		.style("font-size", "16px") 
-        .text("Quarter"); 
+	doViz(data);
 });
 
 // Returns a function to compute the interquartile range.
@@ -142,9 +149,22 @@ function iqr(k) {
   };
 }
 
+function doChartFromJson(json) {
+	var latestData = json[Object.keys(json)[0]];
+	var suites = latestData.suites;
+	var aspectRatios = suites
+		.filter(suite => suite.suite[0] === 'Maintain Aspect Ratio')
+		.map(suite => suite.title);
+
+	var data = aspectRatios
+		.map(ar => [ar.name, ar.results.slice(-30)]);
+
+	doViz(data);
+}
+
 d3.json("benchmarks/latest.json", function(json) {
     // TODO: add a tree (nested list items?) of available benchmarks
-
+	doChartFromJson(json);
 });
 
 fetch('benchmarks/results')
@@ -166,6 +186,7 @@ fetch('benchmarks/results')
                 // then we can click on the square to see historical data
                 // update the square visially to reflect the fact that it is ready
 				historyBox.classList.add('loaded');
+				historyBox.onclick = () => doChartFromJson(json);
             });
         });
     });
